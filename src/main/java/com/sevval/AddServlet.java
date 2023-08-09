1a90f86a6b436ac0883031843ce1d61d0bf59036
baytursevval <baytursevval@gmail.com>
2023-08-09 15:28:36 +0300

    commit

--- src/main/java/com/sevval/AddServlet.java
+++ src/main/java/com/sevval/AddServlet.java
@@ -1,20 +1,32 @@
 package com.sevval;
 
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class AddServlet extends HttpServlet
 {
-	public void doGet(HttpServletRequest req, HttpServletResponse res) {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		int i=Integer.parseInt(req.getParameter("num1"));
 		int j=Integer.parseInt(req.getParameter("num2"));
 		
 		int k=i+j;
 		
-		req.setAttribute("k", k);
+		//req.setAttribute("k", k);
 		
+		//RequestDispatcher
+		//RequestDispatcher rd=req.getRequestDispatcher("sq");
+		//rd.forward(req, res);
+		
+		
+		//SendRedirect
+		res.sendRedirect("sq");
 	}
+	
 
 	
 }
