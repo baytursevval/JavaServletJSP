d575b03361f6c91a4ef2257ef59043ae892082fb
baytursevval <baytursevval@gmail.com>
2023-08-11 13:43:22 +0300

    JSP

--- src/main/java/com/sevval/AddServlet.java
+++ src/main/java/com/sevval/AddServlet.java
@@ -1,15 +1,18 @@
 package com.sevval;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+@WebServlet("/add")
 public class AddServlet extends HttpServlet
 {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
@@ -18,10 +21,17 @@ public class AddServlet extends HttpServlet
 		
 		int k=i+j;
 		
-		Cookie cookie=new Cookie("k", k+"");
-		res.addCookie(cookie);
+		PrintWriter out=res.getWriter();
+		out.println("<html><body bgcolor='cyan'>");
+		out.println("output: "+k);
+		out.println("</body></html>");
 		
-		res.sendRedirect("sq");
+		
+		
+//		Cookie cookie=new Cookie("k", k+"");
+//		res.addCookie(cookie);
+//		
+//		res.sendRedirect("sq");
 		
 //		HttpSession session=req.getSession();
 //		session.setAttribute("k",k);
--- src/main/java/com/sevval/MyServlet.java
+++ src/main/java/com/sevval/MyServlet.java
@@ -5,10 +5,12 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+@WebServlet("/my")
 public class MyServlet extends HttpServlet {
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
